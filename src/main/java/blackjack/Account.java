25ea42685a3f079f7178846e120ffb39affc1bca
WL1237 <winston185@icloud.com>
2026-08-01 18:53:32 +0200

    Instanzvariablen und Konstruktor für die Klasse Account hinzugefügt

---
+++ src/main/java/blackjack/Account.java
@@ -0,0 +1,11 @@
+package blackjack;
+
+public class Account {
+    public String username;
+    public String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
